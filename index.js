a9610bd9be32b4f2e0ba0f3baedb3913d24e0688
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-03-27 11:54:10 +0100

    writefile tbd done

--- index.js
+++ index.js
@@ -53,8 +53,10 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
