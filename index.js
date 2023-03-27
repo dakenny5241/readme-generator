a617949ca0cf3a62b223d51af24201c676ac6583
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-03-27 11:50:05 +0100

    questions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [ 
     {
         type: 'input',
